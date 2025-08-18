b2ae9c065eecc4b2c89bd0c3c5041873192b96ed
Abhishek Dumaniya <abhishekdumaniya196@gmail.com>
2025-08-18 15:25:56 +0530

    fetch openweatherapi based on zipcode

--- src/main/java/com/weatherinfo/controller/weatherInfoController.java
+++ src/main/java/com/weatherinfo/controller/weatherInfoController.java
@@ -1,5 +1,7 @@
 package com.weatherinfo.controller;
 
+import com.weatherinfo.services.weatherInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class weatherInfoController {
 
+    @Autowired
+    weatherInfoService weatherInfoService;
 
-    @GetMapping("/check")
-    public String check(){
-        return "Checking the api";
+
+//    This Controller is used to fetch the openweatherapi api based on the zipcode(pincode)
+    @GetMapping("/weather")
+    public String getWeather(){
+        return weatherInfoService.getOpenWeatherAPI();
     }
-
-    
 
 }
